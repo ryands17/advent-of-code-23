586e171d03936f32ec9956e8a9d3063a875978dd
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-12-03 20:25:56 +0100

    refactor with combinators

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,93 +1,97 @@
+use lazy_static::lazy_static;
 use regex::Regex;
 
-pub fn process(input: &str) -> usize {
-  let re = Regex::new(r"(\d+)").unwrap();
+lazy_static! {
+  static ref MATCHER_REGEX: Regex = Regex::new(r"[^0-9.]").unwrap();
+  static ref NUMBERS_REGEX: Regex = Regex::new(r"(\d+)").unwrap();
+}
 
+pub fn process(input: &str) -> usize {
   let lines = input.lines().collect::<Vec<_>>();
   let vector_start = 0;
   let vector_end = lines.len() - 1;
 
-  let mut sum = 0;
-  for (i, line) in lines.iter().enumerate() {
-    let line_start = 0;
-    let line_end = line.len();
+  let result = lines
+    .iter()
+    .enumerate()
+    .map(|(i, line)| {
+      let line_start = 0;
+      let line_end = line.len();
 
-    let result = re
-      .find_iter(line)
-      .filter_map(|mt| {
-        let start_index = mt.start();
-        let end_index = mt.end();
-        let val = mt.as_str().parse::<usize>().unwrap();
+      NUMBERS_REGEX
+        .find_iter(&line)
+        .filter_map(|mt| {
+          let start_index = mt.start();
+          let end_index = mt.end();
+          let val = mt.as_str().parse::<usize>().unwrap();
 
-        // left
-        if start_index > line_start {
-          let ch = &line[start_index - 1..start_index];
-          if matcher(ch) {
-            return Some(val);
+          // left
+          if start_index > line_start {
+            let ch = &line[start_index - 1..start_index];
+            if matcher(ch) {
+              return Some(val);
+            }
           }
-        }
 
-        // right
-        if end_index < line_end {
-          let ch = &line[end_index..end_index + 1];
-          if matcher(ch) {
-            return Some(val);
+          // right
+          if end_index < line_end {
+            let ch = &line[end_index..end_index + 1];
+            if matcher(ch) {
+              return Some(val);
+            }
           }
-        }
 
-        // top and diagonal
-        if i > vector_start {
-          let start_index = if start_index > line_start {
-            start_index - 1
-          } else {
-            start_index
-          };
+          // top and diagonal
+          if i > vector_start {
+            let start_index = if start_index > line_start {
+              start_index - 1
+            } else {
+              start_index
+            };
 
-          let end_index = if end_index < line_end {
-            end_index + 1
-          } else {
-            end_index
-          };
+            let end_index = if end_index < line_end {
+              end_index + 1
+            } else {
+              end_index
+            };
 
-          let ch = &lines.get(i - 1).unwrap()[start_index..end_index];
-          if matcher(ch) {
-            return Some(val);
+            let ch = &lines.get(i - 1).unwrap()[start_index..end_index];
+            if matcher(ch) {
+              return Some(val);
+            }
           }
-        }
 
-        // bottom and diagonal
-        if i < vector_end {
-          let start_index = if start_index > line_start {
-            start_index - 1
-          } else {
-            start_index
-          };
+          // bottom and diagonal
+          if i < vector_end {
+            let start_index = if start_index > line_start {
+              start_index - 1
+            } else {
+              start_index
+            };
 
-          let end_index = if end_index < line_end {
-            end_index + 1
-          } else {
-            end_index
-          };
+            let end_index = if end_index < line_end {
+              end_index + 1
+            } else {
+              end_index
+            };
 
-          let ch = &lines.get(i + 1).unwrap()[start_index..end_index];
-          if matcher(ch) {
-            return Some(val);
+            let ch = &lines.get(i + 1).unwrap()[start_index..end_index];
+            if matcher(ch) {
+              return Some(val);
+            }
           }
-        }
 
-        None
-      })
-      .sum::<usize>();
+          None
+        })
+        .sum::<usize>()
+    })
+    .sum::<usize>();
 
-    sum += result;
-  }
-
-  sum
+  result
 }
 
 fn matcher(val: &str) -> bool {
-  let re = Regex::new(r"[^0-9.]").unwrap();
-  re.is_match(val)
+  MATCHER_REGEX.is_match(val)
 }
 
 #[cfg(test)]
--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,101 +1,102 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::collections::HashSet;
 
-use regex::Regex;
+lazy_static! {
+  static ref NUMBERS_REGEX: Regex = Regex::new(r"(\d+)").unwrap();
+}
 
 pub fn process(input: &str) -> usize {
-  let re = Regex::new(r"(\d+)").unwrap();
-
   let lines = input.lines().collect::<Vec<_>>();
   let vector_start = 0;
   let vector_end = lines.len() - 1;
 
-  let mut sum = 0;
-  for (i, line) in lines.iter().enumerate() {
-    let line_start = 0;
-    let line_end = line.len();
+  lines
+    .iter()
+    .enumerate()
+    .map(|(i, line)| {
+      let line_start = 0;
+      let line_end = line.len();
 
-    let result = line
-      .match_indices('*')
-      .filter_map(|(pos, _)| {
-        let mut gears = HashSet::new();
+      line
+        .match_indices('*')
+        .filter_map(|(pos, _)| {
+          let mut gears = HashSet::new();
 
-        // check left
-        if pos > line_start {
-          if (&line).chars().nth(pos - 1).unwrap().is_digit(10) {
-            let val = re
-              .find_iter(&line[..pos])
-              .last()
-              .unwrap()
-              .as_str()
-              .parse::<usize>()
-              .unwrap();
+          // check left
+          if pos > line_start {
+            if (&line).chars().nth(pos - 1).unwrap().is_digit(10) {
+              let val = NUMBERS_REGEX
+                .find_iter(&line[..pos])
+                .last()
+                .unwrap()
+                .as_str()
+                .parse::<usize>()
+                .unwrap();
 
-            gears.insert(val);
-          }
-        }
-
-        // check right
-        if pos < line_end {
-          if (&line).chars().nth(pos + 1).unwrap().is_digit(10) {
-            let val = re
-              .find_iter(&line[pos + 1..])
-              .next()
-              .unwrap()
-              .as_str()
-              .parse::<usize>()
-              .unwrap();
-
-            gears.insert(val);
-          }
-        }
-
-        // check top and diagonal
-        if i > vector_start {
-          let top_line = lines.get(i - 1).unwrap();
-          re.find_iter(&top_line).for_each(|mt| {
-            let diagonal = (pos - 1, pos + 1);
-            let start = mt.start();
-            let end = mt.end() - 1;
-
-            if (start <= pos && pos <= end)
-              || (start == diagonal.0 || start == diagonal.1)
-              || (end == diagonal.0 || end == diagonal.1)
-            {
-              gears.insert(mt.as_str().parse::<usize>().unwrap());
+              gears.insert(val);
             }
-          })
-        }
+          }
 
-        // check bottom and diagonal
-        if i < vector_end {
-          let bottom_line = lines.get(i + 1).unwrap();
-          re.find_iter(&bottom_line).for_each(|mt| {
-            let diagonal = (pos - 1, pos + 1);
-            let start = mt.start();
-            let end = mt.end() - 1;
+          // check right
+          if pos < line_end {
+            if (&line).chars().nth(pos + 1).unwrap().is_digit(10) {
+              let val = NUMBERS_REGEX
+                .find_iter(&line[pos + 1..])
+                .next()
+                .unwrap()
+                .as_str()
+                .parse::<usize>()
+                .unwrap();
 
-            if (start <= pos && pos <= end)
-              || (start == diagonal.0 || start == diagonal.1)
-              || (end == diagonal.0 || end == diagonal.1)
-            {
-              gears.insert(mt.as_str().parse::<usize>().unwrap());
+              gears.insert(val);
             }
-          })
-        }
+          }
 
-        if gears.len() == 2 {
-          Some(gears)
-        } else {
-          None
-        }
-      })
-      .map(|gear| gear.iter().fold(1, |acc, val| acc * val))
-      .sum::<usize>();
+          // check top and diagonal
+          if i > vector_start {
+            let top_line = lines.get(i - 1).unwrap();
+            NUMBERS_REGEX.find_iter(&top_line).for_each(|mt| {
+              let diagonal = (pos - 1, pos + 1);
+              let start = mt.start();
+              let end = mt.end() - 1;
 
-    sum += result;
-  }
+              if (start <= pos && pos <= end)
+                || (start == diagonal.0 || start == diagonal.1)
+                || (end == diagonal.0 || end == diagonal.1)
+              {
+                gears.insert(mt.as_str().parse::<usize>().unwrap());
+              }
+            })
+          }
 
-  sum
+          // check bottom and diagonal
+          if i < vector_end {
+            let bottom_line = lines.get(i + 1).unwrap();
+            NUMBERS_REGEX.find_iter(&bottom_line).for_each(|mt| {
+              let diagonal = (pos - 1, pos + 1);
+              let start = mt.start();
+              let end = mt.end() - 1;
+
+              if (start <= pos && pos <= end)
+                || (start == diagonal.0 || start == diagonal.1)
+                || (end == diagonal.0 || end == diagonal.1)
+              {
+                gears.insert(mt.as_str().parse::<usize>().unwrap());
+              }
+            })
+          }
+
+          if gears.len() == 2 {
+            Some(gears)
+          } else {
+            None
+          }
+        })
+        .map(|gear| gear.iter().fold(1, |acc, val| acc * val))
+        .sum::<usize>()
+    })
+    .sum::<usize>()
 }
 
 #[cfg(test)]
