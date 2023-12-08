355077af9a445ed62d599fb7477e4184719f6fae
Ryan Dsouza <r.dsouza@newmotion.com>
2023-12-08 18:16:26 +0100

    refine code

--- day-08/src/part1.rs
+++ day-08/src/part1.rs
@@ -12,19 +12,10 @@ pub fn process(input: &str) -> usize {
       let mut sp = line.split('=');
 
       let key = sp.next().unwrap().trim();
-
-      let coordinates = sp
-        .next()
-        .unwrap()
-        .trim()
-        .chars()
-        .filter(|c| c != &'(' && c != &')')
-        .collect::<String>();
-
-      let mut sp = coordinates.split(',');
+      let mut coordinates = sp.next().unwrap().trim()[1..][..8].split(',');
       let value = (
-        sp.next().unwrap().trim().to_string(),
-        sp.next().unwrap().trim().to_string(),
+        coordinates.next().unwrap().trim(),
+        coordinates.next().unwrap().trim(),
       );
 
       (key, value.to_owned())
@@ -38,17 +29,12 @@ pub fn process(input: &str) -> usize {
   while start_val != "ZZZ" {
     let val = mappings.get(&start_val).unwrap();
     let direction = pattern.get(pointer).unwrap();
-    if direction == &'L' {
-      start_val = &val.0;
-    } else {
-      start_val = &val.1;
-    }
+    start_val = if direction == &'L' { &val.0 } else { &val.1 };
+
     pointer += 1;
     steps += 1;
 
-    if pointer == end {
-      pointer = 0;
-    }
+    pointer = if pointer == end { 0 } else { pointer };
   }
 
   steps
--- day-08/src/part2.rs
+++ day-08/src/part2.rs
@@ -13,22 +13,13 @@ pub fn process(input: &str) -> usize {
       let mut sp = line.split('=');
 
       let key = sp.next().unwrap().trim();
-
-      let coordinates = sp
-        .next()
-        .unwrap()
-        .trim()
-        .chars()
-        .filter(|c| c != &'(' && c != &')')
-        .collect::<String>();
-
-      let mut sp = coordinates.split(',');
+      let mut coordinates = sp.next().unwrap().trim()[1..][..8].split(',');
       let value = (
-        sp.next().unwrap().trim().to_string(),
-        sp.next().unwrap().trim().to_string(),
+        coordinates.next().unwrap().trim(),
+        coordinates.next().unwrap().trim(),
       );
 
-      (key, value.to_owned())
+      (key, value)
     })
     .collect::<HashMap<_, _>>();
 
@@ -43,16 +34,11 @@ pub fn process(input: &str) -> usize {
       while !start_val.ends_with('Z') {
         let val = mappings.get(&start_val).unwrap();
         let direction = pattern.get(pointer).unwrap();
-        if direction == &'L' {
-          start_val = &val.0;
-        } else {
-          start_val = &val.1;
-        }
+        start_val = if direction == &'L' { &val.0 } else { &val.1 };
+
         pointer += 1;
         steps += 1;
-        if pointer == end {
-          pointer = 0;
-        }
+        pointer = if pointer == end { 0 } else { pointer };
       }
       steps
     })
